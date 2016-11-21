01b16420dc20e1a483138f4c2932b0707c62561b
JayZ <JayZ@JayZs-MacBook-Pro.local>
2016-11-21 14:45:06 -0700

    Add Exercise 1 of 38

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main(String[] args ) {
+		System.out.println( "I, Joshua Zink, am determined to learn code. " );
+		System.out.println( "Today's date is Mon 21 Nov 2016" );
+	}
+}
