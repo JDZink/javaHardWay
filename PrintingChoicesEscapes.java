4f775d02ebe8debee29fa5a31007123d17c974df
JayZ <JayZ@JayZs-MacBook-Pro.local>
2016-11-21 16:17:39 -0700

    Escape Sequences 4 of 38

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,24 @@
+public class PrintingChoicesEscapes {
+	public static void main(String[] args){
+		System.out.println("Alpha\nBravo\nCharlie\nDelts\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test\nI am learning Java the Hard Way!");
+		/*System.out.println("Bravo");
+		
+		System.out.println("Charlie");
+		System.out.println("Delta");
+		System.out.println();
+		
+		System.out.print("Echo");
+		System.out.print("Foxtrot");
+		
+		System.out.println( "Golf");
+		System.out.print( "Hotel");
+		System.out.println();
+		System.out.println("India");
+		
+		System.out.println();
+		System.out.println( "This" + " " + "is" + " " + "a" + " test");
+		System.out.println("I am learn" + "ing Java the H" + "ar" + "d Way!");*/
+		
+	}
+}
+	
