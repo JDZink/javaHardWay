ea9d420c4c10c4e26c3ef0e4baa5759e7585e565
JayZ <JayZ@JayZs-MacBook-Pro.local>
2016-11-21 15:16:35 -0700

    Adding Exercise 2 of 38

---
+++ GasolineReciept.java
@@ -0,0 +1,18 @@
+public class GasolineReciept {
+	public static void main(String[] args){
+		System.out.println("+------------------------+");
+		String blankLine = "|                        |";
+		System.out.println(blankLine);
+		System.out.println("|      CORNER STORE      |");
+		System.out.println(blankLine);
+		System.out.println("| 2016-11-21 02:49PM     |");
+		System.out.println(blankLine);
+		System.out.println("| Gallons:       10.870  |");
+		System.out.println("| Price/Gallon: $ 2.109  |");
+		System.out.println(blankLine);
+		System.out.println("| Fuel total:  $ 22.92   |");
+		System.out.println(blankLine);
+		System.out.println("+------------------------+");
+			
+	}
+}
