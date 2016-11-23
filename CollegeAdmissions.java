e2bc29f21c18e69b8685c1bed4a667ce1e7470ef
JayZ <JayZ@JayZs-MacBook-Pro.local>
2016-11-22 18:57:31 -0700

    17-20 of 38

---
+++ CollegeAdmissions.java
@@ -0,0 +1,30 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class CollegeAdmissions {
+	public static void main(String[] args){
+		Scanner keys = new Scanner(System.in);
+		int math;
+		
+		out.println("Welcome to the UT Austin College Admissions Intterface!");
+		out.print( "Please enter your SAT math score (200-800): ");
+		math = keys.nextInt();
+		
+		out.print( "Admittance status: " );
+		
+		if (math >= 790)
+			out.print("CERTAIN ");
+		else if (math >= 390)
+			out.print ( "UNLIKELY ");
+		else if (math >= 710)
+			out.print("SAFE ");
+		else if (math >= 580 ) 
+			out.print("PROBABLE ");
+		else if (math >= 500)
+			out.print("UNCERTAIN ");
+		else //below 390
+			out.print("DENIED ");
+			out.println();
+			
+	}
+}
